e77c274a525b0fdc498627701810eface8c6240a
soulsystem00 <63180108+soulsystem00@users.noreply.github.com>
2021-07-18 17:45:33 +0900

    add MainMenu

---
+++ Assets/Script/ButtonEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonEvent : MonoBehaviour, IPointerClickHandler
+{
+
+    public enum ButtonType
+    {
+        GameStart,
+
+    }
+
+    public ButtonType buttonType;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(buttonType == ButtonType.GameStart)
+        {
+            SceneManager.LoadScene("BaseCamp_Outside");
+        }
+    }
+}
